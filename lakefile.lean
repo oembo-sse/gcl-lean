8bc74d9f02182b51d16fd8f3356e6641d9983389
oeb25 <oliverboving@gmail.com>
2025-04-28 10:07:06 +0200

    update ci and add docs to subfolder

--- lakefile.lean
+++ lakefile.lean
@@ -11,11 +11,7 @@ package «gcl» where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
-require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4"@"v0.0.53"
 
 @[default_target]
 lean_lib «Gcl» where
   -- add any library configuration options here
-
-meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
